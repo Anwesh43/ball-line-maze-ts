713a8813a347b83a381f80785169dd4152d9c51a
Anwesh43 <anweshthecool0@gmail.com>
2019-02-16 17:56:03 +0530

    adding scale methods

--- BallLineMazeStage.ts
+++ BallLineMazeStage.ts
@@ -7,6 +7,25 @@ const strokeFactor : number = 3
 const foreColor : string = '#4CAF50'
 const backColor : string = '#BDBDBD'
 
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) =>  Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class BallLineMazeStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
