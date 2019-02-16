c13b9fec2e6f0d722e03b910362f26ffddaf5931
Anwesh43 <anweshthecool0@gmail.com>
2019-02-16 18:33:48 +0530

    adding BLMNode class

--- BallLineMazeStage.ts
+++ BallLineMazeStage.ts
@@ -131,3 +131,48 @@ class Animator {
         }
     }
 }
+
+class BLMNode {
+
+    state : State = new State()
+    prev : BLMNode
+    next : BLMNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new BLMNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawBLMNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BLMNode {
+        var curr : BLMNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
